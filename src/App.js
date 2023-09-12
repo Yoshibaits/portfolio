dd2ee4561a7a7877f872ca13b550ef58d10fb5e5
Yoshibaits <neil_dsg@yahoo.com>
2023-09-12 21:57:57 +0800

    navbar small design update

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-
+import Navbar from "./components/navbar/navbar";
 
 function App() {
   
   return (
     <div className="App">
-
+        <Navbar/>
     </div>
   );
 }
---
+++ src/components/navbar/navbar.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import './navbar.css'
+import Logo from '../../assets/logo.png'
+import contactImg from '../../assets/contact.png'
+import {Link} from 'react-scroll'
+
+const navbar = () => {
+  return (
+        <nav className='navbar'>
+            <img src={Logo} alt='Logo' className='logo'/>
+            <div className='desktopMenu'> 
+                <Link className="desktopMenuListItem">Home</Link>
+                <Link className="desktopMenuListItem">About</Link>
+                <Link className="desktopMenuListItem">Portfolio</Link>
+                <Link className="desktopMenuListItem">Client</Link>
+            </div>
+            <button className="desktopMenubtn">
+                <img src={contactImg} alt="" className="desktopMenuImg" />Contact Me
+            </button>
+        </nav>
+
+  )
+}
+
+export default navbar
