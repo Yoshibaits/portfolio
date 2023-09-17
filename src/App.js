7ec9e7d95916986948496fe4608735847d7115ec
Yoshibaits <neil_dsg@yahoo.com>
2023-09-17 20:09:23 +0800

    start of what i do Section

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import Navbar from "./components/navbar/navbar";
 import Intro from './components/Intro/Intro'
+import Skills from "./components/Skills/skills";
 
 function App() {
   
@@ -7,6 +8,7 @@ function App() {
     <div className="App">
         <Navbar/>
         <Intro/>
+        <Skill />
     </div>
   );
 }
